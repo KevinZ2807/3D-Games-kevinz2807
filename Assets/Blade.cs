c7d6cee93174b56a1e47165b587dade4aea20a59
KevinZ2807 <kietbdt@gmail.com>
2024-01-23 15:56:35 +0700

    Add Blade and slicing function

---
+++ Assets/Blade.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Collider bladeCollider;
+    
+    private BladeState currentState;
+    public Vector3 direction { get; private set;}
+    private float minSliceVelocity = 0.01f;
+
+    private enum BladeState {
+        Idle,
+        Starting,
+        Slicing,
+        Stopping
+    }
+
+    void Start() {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+        currentState = BladeState.Idle;
+    }
+
+    private void Update() {
+        switch (currentState) {
+            case BladeState.Idle:
+                CheckStartSlicing();
+                break;
+            case BladeState.Starting:
+                StartSlicing();
+                break;
+            case BladeState.Slicing:
+                ContinueSlicing();
+                break;
+            case BladeState.Stopping:
+                StopSlicing();
+                break;
+        }   
+    }
+
+    private void OnEnable() {
+        StopSlicing();
+    }
+
+    private void OnDisable() {
+        StopSlicing();
+    }
+    private void CheckStartSlicing() {
+        if (Input.GetMouseButtonDown(0)) {
+            currentState = BladeState.Starting;
+        }
+    }
+
+    private void StartSlicing() {
+        SetNewPosition();
+        currentState = BladeState.Slicing;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopSlicing() {
+        currentState = BladeState.Idle;
+        bladeCollider.enabled = false;
+    }
+
+    private void ContinueSlicing() {
+        if (Input.GetMouseButtonUp(0)) {
+            currentState = BladeState.Stopping;
+        } else {
+            SetNewPosition();
+
+            float velocity = direction.magnitude / Time.deltaTime;
+            bladeCollider.enabled = velocity > minSliceVelocity;
+        }
+    }
+
+    private void SetNewPosition() {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+        transform.position = newPosition;
+    }
+}
