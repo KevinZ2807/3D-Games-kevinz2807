00a47e4eea1000b959059e92a9522bf06de7e697
KevinZ2807 <kietbdt@gmail.com>
2024-01-23 21:06:33 +0700

    Modify Update + Reset High score function

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI highScoreText;
     private int score = 0;
+    private float timeCount = 0f;
 
     private void Awake() {
         highScoreText.text = "High score: " + PlayerPrefs.GetInt("HighScore").ToString();
@@ -19,6 +20,30 @@ public class GameManager : MonoBehaviour
         NewGame();
     }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("R pressed");
+            timeCount = 0f; // Reset time count whenever R is pressed
+        }
+
+        // Check if R key is being held down
+        if (Input.GetKey(KeyCode.R)) {
+            timeCount += Time.deltaTime;
+            
+        // Check if the key has been held down for 5 seconds
+            if (timeCount >= 5.0f) {
+                ResetHighScore();
+                UpdateHighScore();
+                timeCount = 0f; // Reset time count after action is performed
+            }
+        }
+
+    // Reset the time count when R key is released
+        if (Input.GetKeyUp(KeyCode.R)) {
+            timeCount = 0f;
+        }
+    }
+
     private void NewGame() {
         score = 0;
         scoreText.text = "Score: " + score.ToString();
@@ -27,11 +52,14 @@ public class GameManager : MonoBehaviour
     public void IncreaseScore(int point) {
         score += point;
         scoreText.text = "Score: " + score.ToString();
+        UpdateHighScore();
     }
 
     public void UpdateHighScore() {
         if (score > PlayerPrefs.GetInt("HighScore", 0)) {
             PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = "High score: " + score.ToString();
+        } else {
             highScoreText.text = "High score: " + score.ToString();
         }
     }
