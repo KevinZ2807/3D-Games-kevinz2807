1da94b514e0fca621e64f4ad6807c6f84f12dfb8
KevinZ2807 <kietbdt@gmail.com>
2024-02-21 14:17:35 +0700

    Add texture and prefabs to the game + coding

---
+++ Assets/Scripts/PongGame/RacketController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [Header("Attributes")]
+    [SerializeField] private float speed;
+    
+    private float vertical;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = Vector3.forward * speed * vertical;
+    }
+}
