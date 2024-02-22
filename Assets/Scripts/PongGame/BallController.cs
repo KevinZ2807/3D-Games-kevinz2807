1df17f618e516d4d23ff08e94c1bad32437d652e
KevinZ2807 <kietbdt@gmail.com>
2024-02-22 15:06:06 +0700

    feat: Improve Ball behavior + Add Racket animation
    Ball behavior: If collide to racket, it can randomly ricochet to random direction (At least I think so)

--- Assets/Scripts/PongGame/BallController.cs
+++ Assets/Scripts/PongGame/BallController.cs
@@ -14,6 +14,7 @@ public class BallController : MonoBehaviour
     
     private float xRandom;
     private float zRandom;
+    private float minDirection = 0.5f;
     
     void Start()
     {
@@ -33,10 +34,21 @@ public class BallController : MonoBehaviour
 
 
     private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Player") {
+            Vector3 newDirection = (transform.position - other.transform.position).normalized;
+
+            // Mathf.Sign: Return a value 0, -1 or 1 depends on the "x" value is negative or positive or zero
+            // Mathf.Max: Return the largest value between 2 values
+            newDirection.x = Mathf.Sign(newDirection.x) * Mathf.Max(Mathf.Abs(newDirection.x), minDirection);
+            newDirection.z = Mathf.Sign(newDirection.z) * Mathf.Max(Mathf.Abs(newDirection.z), minDirection);
+            direction = newDirection;
+            return;
+        }
         var firstContact = other.contacts[0];
         Debug.Log(other.contacts[0].normal);
         Vector3 newVelocity = Vector3.Reflect(direction.normalized, firstContact.normal); // Reflect to the wall it hits
         direction = newVelocity; // Change direction base on the reflection
+        
     }
 
 }
