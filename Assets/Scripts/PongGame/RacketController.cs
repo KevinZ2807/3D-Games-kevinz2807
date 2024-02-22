55154a6ca36c46339ef1059806fa73d68c638c93
KevinZ2807 <kietbdt@gmail.com>
2024-02-22 16:29:59 +0700

    feat: Add AI to the game

--- Assets/Scripts/PongGame/RacketController.cs
+++ Assets/Scripts/PongGame/RacketController.cs
@@ -6,27 +6,55 @@ public class RacketController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform ball;
     [Header("Attributes")]
     [SerializeField] private float speed;
+    [SerializeField] private bool isPlayer;
+    [SerializeField] private float offset = 2f;
     public KeyCode up;
     public KeyCode down;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ball = GameObject.FindGameObjectWithTag("Ball").transform;
     }
 
     private void FixedUpdate()
     {
-        MovePlayer(up, down);
+        if (isPlayer) {
+            MoveByPlayer(up, down);
+        } else {
+            MoveByComputer();
+        }
     }
 
-    private void MovePlayer(KeyCode upKey, KeyCode downKey)
+    private void MoveByPlayer(KeyCode upKey, KeyCode downKey)
     {
         var direction = 0f;
         if (Input.GetKey(upKey)) direction += 1f;
         if (Input.GetKey(downKey)) direction -= 1f;
+        if (!Input.GetKey(upKey) && !Input.GetKey(downKey)) {
+            rb.velocity = Vector3.zero;
+            return;
+        }
 
-        transform.position += Vector3.forward * direction * speed * Time.deltaTime;
+        rb.velocity += Vector3.forward * direction * speed * Time.deltaTime;
+    }
+
+
+    private void MoveByComputer() {
+        if (ball.position.z != transform.position.z) {
+        // Determine the direction to move in
+        Vector3 moveDirection = (ball.position.z > transform.position.z 
+        + Random.Range(-offset, offset)) ? Vector3.forward : Vector3.back;
+        
+        // Apply the movement
+        rb.velocity = moveDirection * speed;
+        }
+        else {
+            // Optionally, stop the movement if the positions are equal
+            rb.velocity = Vector3.zero;
+        }
     }
 }
